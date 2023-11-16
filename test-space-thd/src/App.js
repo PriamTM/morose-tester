56eec5b6bc74cd09210590c471c33735d2dca84f
PriamTM <33031471+PriamTM@users.noreply.github.com>
2023-11-16 19:51:41 +0000

    Pending changes exported from your codespace

--- test-space-thd/src/App.js
+++ test-space-thd/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Master Branch
+          Master Branches
         </p>
       </header>
     </div>
