1a05ed5ec40eb6821ca021cc7686b5e86594a9e8
PriamTM <33031471+PriamTM@users.noreply.github.com>
2023-11-21 20:19:55 +0000

    codeswings

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -1,15 +1,15 @@
 import '../App.css';
 
-function Card() {
+function Button() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Card
+          Button edited
         </p>
       </header>
     </div>
   );
 }
 
-export { Card };
+export { Button };
--- src/checkbox/App.jsx
+++ src/checkbox/App.jsx
@@ -12,4 +12,4 @@ function Checkbox() {
   );
 }
 
-export { Checkbox };
+export default Checkbox;
